5f0e33696dd1529edb5ad823ad474e526f8762b9
Ian Dela Cruz <iandc76@gmail.com>
2019-09-27 11:47:58 +0800

    feat(admin): initial next.js codes

---
+++ pages/admin/index.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <h1>Welcome to KDC CMS</h1>;
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const Koa = require("koa");
+const Router = require("@koa/router");
 const responseTime = require("koa-response-time");
+const next = require("next");
 
 const api = require("./api/index");
 
 const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const server = new Koa();
+app.prepare().then(() => {
+  const server = new Koa();
+  const router = new Router();
 
-server.use(responseTime());
+  server.use(responseTime());
 
-server.use(api.routes());
+  router.get("/admin", async ctx => {
+    await app.render(ctx.req, ctx.res, "/admin", ctx.query);
+    ctx.respond = false;
+  });
 
-server.listen(port);
+  router.all("/admin", async ctx => {
+    await handle(ctx.req, ctx.res);
+    ctx.respond = false;
+  });
+
+  server.use(router.routes());
+  server.use(api.routes());
+
+  server.listen(port);
+});
