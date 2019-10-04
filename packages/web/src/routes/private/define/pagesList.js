d08cdfc2e9f41e0e5699827ff5e24b3b0d987778
Ian Dela Cruz <iandc76@gmail.com>
2019-10-05 07:48:05 +0800

    feat(web): list pages from api

--- packages/web/src/routes/private/define/pagesList.js
+++ packages/web/src/routes/private/define/pagesList.js
@@ -1,40 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from '@reach/router';
 import { Card, CardBody, CardHeader, Table } from 'reactstrap';
+import api from '../../../utils/api';
 
-const PagesList = () => (
-  <Card>
-    <CardHeader className="d-flex justify-content-between">
-      <h3 className="m-0">List</h3>
-      <Link to="add" className="btn btn-sm btn-primary">
-        Add Page
-      </Link>
-    </CardHeader>
-    <CardBody>
-      <Table hover striped responsive>
-        <thead>
-          <tr>
-            <th>id</th>
-            <th>Page Name</th>
-            <th>Description</th>
-            <th>Fields</th>
-            <th>Last Edited</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">home</th>
-            <td>Home Page</td>
-            <td>Define home page details</td>
-            <td>3</td>
-            <td>Oct 4, 2019</td>
-            <td>Edit</td>
-          </tr>
-        </tbody>
-      </Table>
-    </CardBody>
-  </Card>
-);
+const PagesList = () => {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const fetchList = async () => api('page-definition').then((data) => setList(data));
+    fetchList();
+  }, []);
+
+  return (
+    <Card>
+      <CardHeader className="d-flex justify-content-between">
+        <h3 className="m-0">List</h3>
+        <Link to="add" className="btn btn-sm btn-primary">
+          Add Page
+        </Link>
+      </CardHeader>
+      <CardBody>
+        <Table hover striped responsive>
+          <thead>
+            <tr>
+              <th>id</th>
+              <th>Page Name</th>
+              <th>Description</th>
+              <th>Fields</th>
+              <th>Last Edited</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((page) => (
+              <tr key={page.id}>
+                <th scope="row">{page.id}</th>
+                <td>{page.name}</td>
+                <td>{page.description}</td>
+                <td>{page.fieldCount}</td>
+                <td>{page.createdAt}</td>
+                <td>Edit</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </CardBody>
+    </Card>
+  );
+};
 
 export default PagesList;
