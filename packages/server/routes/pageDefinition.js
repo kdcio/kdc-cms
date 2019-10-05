480e763f7e938104fc1b74045eae08ace0015486
Ian Dela Cruz <iandc76@gmail.com>
2019-10-05 09:45:03 +0800

    feat(api): Added error handling on pages

--- packages/server/routes/pageDefinition.js
+++ packages/server/routes/pageDefinition.js
@@ -5,38 +5,63 @@ const PageDefinition = require('../models/pageDefinition');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const list = await PageDefinition.list();
-  res.status(HttpStatus.OK);
-  res.send(list);
+  try {
+    const list = await PageDefinition.list();
+    res.status(HttpStatus.OK);
+    res.send(list);
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.post('/', async (req, res) => {
   const { body } = req;
-  const id = await PageDefinition.post(body);
-  res.status(HttpStatus.CREATED);
-  res.send({ id });
+  try {
+    const id = await PageDefinition.post(body);
+    res.status(HttpStatus.CREATED);
+    res.send({ id });
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { body } = req;
-  await PageDefinition.put({ id, attr: body });
-  res.status(HttpStatus.NO_CONTENT);
-  res.send();
+  try {
+    await PageDefinition.put({ id, attr: body });
+    res.status(HttpStatus.NO_CONTENT);
+    res.send();
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  await PageDefinition.delete({ id });
-  res.status(HttpStatus.NO_CONTENT);
-  res.send();
+  try {
+    await PageDefinition.delete({ id });
+    res.status(HttpStatus.NO_CONTENT);
+    res.send();
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const item = await PageDefinition.get({ id });
-  res.status(HttpStatus.OK);
-  res.send(item);
+  try {
+    const item = await PageDefinition.get({ id });
+    res.status(HttpStatus.OK);
+    res.send(item);
+  } catch (error) {
+    res.status(HttpStatus.NOT_FOUND);
+    res.send(error);
+  }
 });
 
 module.exports = router;
--- packages/server/routes/pages.js
+++ packages/server/routes/pages.js
@@ -5,38 +5,63 @@ const Pages = require('../models/pages');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const list = await Pages.list();
-  res.status(HttpStatus.OK);
-  res.send(list);
+  try {
+    const list = await Pages.list();
+    res.status(HttpStatus.OK);
+    res.send(list);
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.post('/', async (req, res) => {
   const { body } = req;
-  const id = await Pages.post(body);
-  res.status(HttpStatus.CREATED);
-  res.send({ id });
+  try {
+    const id = await Pages.post(body);
+    res.status(HttpStatus.CREATED);
+    res.send({ id });
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { body } = req;
-  await Pages.put({ id, attr: body });
-  res.status(HttpStatus.NO_CONTENT);
-  res.send();
+  try {
+    await Pages.put({ id, attr: body });
+    res.status(HttpStatus.NO_CONTENT);
+    res.send();
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  await Pages.delete({ id });
-  res.status(HttpStatus.NO_CONTENT);
-  res.send();
+  try {
+    await Pages.delete({ id });
+    res.status(HttpStatus.NO_CONTENT);
+    res.send();
+  } catch (error) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR);
+    res.send(error);
+  }
 });
 
 router.get('/:id', async (req, res) => {
   const { id } = req.params;
-  const item = await Pages.get({ id });
-  res.status(HttpStatus.OK);
-  res.send(item);
+  try {
+    const item = await Pages.get({ id });
+    res.status(HttpStatus.OK);
+    res.send(item);
+  } catch (error) {
+    res.status(HttpStatus.NOT_FOUND);
+    res.send(error);
+  }
 });
 
 module.exports = router;
