758911c89f7bdda705f18f3c75dc64fb287576e5
Ian Dela Cruz <iandc76@gmail.com>
2019-10-09 10:12:17 +0800

    feat(web): Mandatory name, slug and sort key field in type

--- packages/web/src/routes/private/define/typesForm.js
+++ packages/web/src/routes/private/define/typesForm.js
@@ -3,35 +3,46 @@ import PropTypes from 'prop-types';
 import { Link, navigate } from '@reach/router';
 import useForm from 'react-hook-form';
 import slugify from 'slugify';
+import find from 'lodash.find';
 import { Col, Card, CardBody, CardHeader, Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import api from '../../../utils/api';
 
 const createArrayWithNumbers = (length) => Array.from({ length }, (_, k) => k);
 
 const TypesForm = ({ id }) => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, watch } = useForm();
   const [size, setSize] = useState(1);
   const [initialValues, setInitialValues] = useState({});
-  const onSubmit = ({ name, description, field, type }) => {
+  const onSubmit = ({ name, description, field, type, sortKey }) => {
     const body = {
       name: name.trim(),
       id: id || slugify(name, { lower: true }),
       fields: [],
-      fieldCount: 0,
+      fieldCount: 2, // name and Slug
+      sortKey,
     };
 
     if (description.trim() !== '') {
       body.description = description.trim();
     }
 
+    body.fields.push({ name: 'Name', type: 'Text' });
+    body.fields.push({ name: 'Slug', type: 'Text' });
+
     field.forEach((v, k) => {
       if (v.trim() === '') return;
-      body.fields.push({
-        name: v.trim(),
-        type: type[k],
-      });
+      body.fields.push({ name: v.trim(), type: type[k] });
       body.fieldCount += 1;
     });
+
+    // check if sort key exists
+    if (!find(body.fields, { name: sortKey })) {
+      /**
+       * TODO: Add error handler
+       */
+      // console.log('sort key field not found');
+      return;
+    }
 
     if (id) {
       api(`content-definition/${id}`, { body, method: 'PUT' }).then(() => {
@@ -47,9 +58,20 @@ const TypesForm = ({ id }) => {
 
     api(`content-definition/${id}`).then((data) => {
       setInitialValues(data);
-      setSize(data.fieldCount);
+      setSize(data.fieldCount - 2); // Subtract Name & Slug
     });
   }, [id]);
+
+  let initialFields = [];
+  const { fields } = initialValues;
+  if (fields) {
+    initialFields = fields.filter((v) => {
+      if (v.name === 'Name' || v.name === 'Slug') return false;
+      return true;
+    });
+  }
+
+  const field = watch('field');
 
   return (
     <Card>
@@ -83,39 +105,60 @@ const TypesForm = ({ id }) => {
           </FormGroup>
           <hr />
           <h4>Fields</h4>
-          {createArrayWithNumbers(size).map((number) => (
-            <FormGroup key={number} row>
-              <Label sm={2}>Field Name</Label>
-              <Col sm={6}>
-                <Input
-                  type="text"
-                  name={`field[${number}]`}
-                  innerRef={register}
-                  defaultValue={
-                    initialValues && initialValues.fields && initialValues.fields[number]
-                      ? initialValues.fields[number].name
-                      : ''
-                  }
-                />
-              </Col>
-              <Label sm={2}>Field Type</Label>
-              <Col sm={2}>
-                <Input
-                  type="select"
-                  name={`type[${number}]`}
-                  innerRef={register}
-                  defaultValue={
-                    initialValues && initialValues.fields && initialValues.fields[number]
-                      ? initialValues.fields[number].type
-                      : ''
-                  }
-                >
-                  <option value="text">Text</option>
-                  <option value="long-text">Long Text</option>
-                </Input>
-              </Col>
-            </FormGroup>
-          ))}
+          <FormGroup row>
+            <Label sm={2}>Field Name</Label>
+            <Col sm={6}>
+              <Input type="text" name="cName" innerRef={register} value="Name" readOnly />
+            </Col>
+            <Label sm={2}>Field Type</Label>
+            <Col sm={2}>
+              <Input type="text" name="cNameType" innerRef={register} value="Text" readOnly />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label sm={2}>Field Name</Label>
+            <Col sm={6}>
+              <Input type="text" name="slug" innerRef={register} value="Slug" readOnly />
+            </Col>
+            <Label sm={2}>Field Type</Label>
+            <Col sm={2}>
+              <Input type="text" name="slugType" innerRef={register} value="Text" readOnly />
+            </Col>
+          </FormGroup>
+          {createArrayWithNumbers(size).map((number) => {
+            let defName = '';
+            let defType = 'Text';
+
+            if (initialFields && initialFields[number]) {
+              defName = initialFields[number].name;
+              defType = initialFields[number].type;
+            }
+            return (
+              <FormGroup key={number} row>
+                <Label sm={2}>Field Name</Label>
+                <Col sm={6}>
+                  <Input
+                    type="text"
+                    name={`field[${number}]`}
+                    innerRef={register}
+                    defaultValue={defName}
+                  />
+                </Col>
+                <Label sm={2}>Field Type</Label>
+                <Col sm={2}>
+                  <Input
+                    type="select"
+                    name={`type[${number}]`}
+                    innerRef={register}
+                    defaultValue={defType}
+                  >
+                    <option value="text">Text</option>
+                    <option value="long-text">Long Text</option>
+                  </Input>
+                </Col>
+              </FormGroup>
+            );
+          })}
           <div className="d-flex justify-content-end">
             <Button
               type="button"
@@ -133,6 +176,39 @@ const TypesForm = ({ id }) => {
               Remove
             </Button>
           </div>
+          <hr />
+          <FormGroup row>
+            <Label sm={2}>Sort by</Label>
+            <Col sm={4}>
+              {id ? (
+                <Input
+                  type="text"
+                  name="sortKey"
+                  innerRef={register}
+                  defaultValue={initialValues.sortKey}
+                  readOnly
+                />
+              ) : (
+                <Input
+                  type="select"
+                  name="sortKey"
+                  innerRef={register}
+                  defaultValue={initialValues.sortKey}
+                >
+                  <option value="name">Name</option>
+                  {field
+                    && field.map((v) => {
+                      if (v.trim() === '') return null;
+                      return (
+                        <option key={v} value={v}>
+                          {v}
+                        </option>
+                      );
+                    })}
+                </Input>
+              )}
+            </Col>
+          </FormGroup>
           <hr />
           <Button type="submit" color="primary">
             Save
