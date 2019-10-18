475b62caf787ed6ab529aee21c35f444d212b624
Ian Dela Cruz <iandc76@gmail.com>
2019-10-19 07:09:33 +0800

    test(api): use different table for unit testing

--- packages/kdc-cms-api/jest.setup.js
+++ packages/kdc-cms-api/jest.setup.js
@@ -1,6 +1,8 @@
-import { clearByGSI } from 'kdc-cms-dynamodb';
+import { createTable, clearByGSI } from 'kdc-cms-dynamodb';
+import schema from '../../resources/database/schema.json';
 
 export default async () => {
+  await createTable(schema);
   await clearByGSI('page');
   await clearByGSI('page#data');
   await clearByGSI('content');
