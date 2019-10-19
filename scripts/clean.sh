eff335af87e3887a1b78efa45f5714236b099f8d
Ian Dela Cruz <iandc76@gmail.com>
2019-10-19 08:23:37 +0800

    feat(all): added script to remove all non-git folders

---
+++ scripts/clean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -fR node_modules
+rm -fR resources/database/.dbdata
+rm -fR resources/database/.serverless
+rm -fR packages/kdc-cms-admin/node_modules
+rm -fR packages/kdc-cms-admin/.serverless
+rm -fR packages/kdc-cms-api/node_modules
+rm -fR packages/kdc-cms-api/.serverless
+rm -fR packages/kdc-cms-dynamodb/node_modules
+rm -fR packages/kdc-cms-setup/node_modules
+rm -fR packages/kdc-cms-utils/node_modules
