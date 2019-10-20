54629320c66f3545af548e92f28d543f8a21b140
Ian Dela Cruz <iandc76@gmail.com>
2019-10-20 12:19:19 +0800

    refactor(admin): move fontawesome to separate file

--- packages/kdc-cms-admin/src/App.js
+++ packages/kdc-cms-admin/src/App.js
@@ -1,19 +1,5 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faAngleLeft,
-  faAngleRight,
-  faAngleUp,
-  faBars,
-  faKeyboard,
-  faTachometerAlt,
-  faFile,
-  faFolder,
-  faCog,
-  faWrench,
-  faUser,
-  faSignOutAlt,
-} from '@fortawesome/free-solid-svg-icons';
+import './fa'; // fontawesome
 import { useAuth } from './context/auth';
 import { SideBarToggleProvider } from './context/sideBar';
 import { ContentTypeListProvider } from './context/contentTypeList';
@@ -22,21 +8,6 @@ import FullPageSpinner from './components/fullPageSpinner';
 const loadPrivateApp = () => import('./routes/private');
 const PrivateApp = React.lazy(loadPrivateApp);
 const PublicApp = React.lazy(() => import('./routes/public'));
-
-library.add(
-  faAngleLeft,
-  faAngleRight,
-  faAngleUp,
-  faBars,
-  faKeyboard,
-  faTachometerAlt,
-  faFile,
-  faFolder,
-  faCog,
-  faWrench,
-  faUser,
-  faSignOutAlt
-);
 
 function App() {
   const { user } = useAuth();
---
+++ packages/kdc-cms-admin/src/fa.js
@@ -0,0 +1,32 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faAngleUp,
+  faBars,
+  faKeyboard,
+  faTachometerAlt,
+  faFile,
+  faFolder,
+  faCog,
+  faWrench,
+  faUser,
+  faSignOutAlt,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(
+  faAngleLeft,
+  faAngleRight,
+  faAngleUp,
+  faBars,
+  faKeyboard,
+  faTachometerAlt,
+  faFile,
+  faFolder,
+  faCog,
+  faWrench,
+  faUser,
+  faSignOutAlt,
+  faUsers
+);
