ec5b73874e5e092e68facaee98accc8a5bf1b221
Ian Dela Cruz <iandc76@gmail.com>
2019-10-21 14:51:38 +0800

    feat(admin): lazy load routes
    issue #6

--- packages/kdc-cms-admin/src/routes/private/index.js
+++ packages/kdc-cms-admin/src/routes/private/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Router } from '@reach/router';
-import Dashboard from './dashboard';
-import DefinePages from './define/pages';
-import DefineTypes from './define/types';
-import Pages from './pages/index';
-import Contents from './contents/index';
-import Users from './users/index';
-import Error404 from './404';
+import FullPageSpinner from '../../components/fullPageSpinner';
+
+const Dashboard = React.lazy(() => import('./dashboard'));
+const DefinePages = React.lazy(() => import('./define/pages'));
+const DefineTypes = React.lazy(() => import('./define/types'));
+const Pages = React.lazy(() => import('./pages'));
+const Contents = React.lazy(() => import('./contents'));
+const Users = React.lazy(() => import('./users'));
+const Error404 = React.lazy(() => import('./404'));
 
 const Public = () => (
-  <Router>
-    <Dashboard path="/" />
-    <DefinePages path="/define/pages/*" />
-    <DefineTypes path="/define/types/*" />
-    <Pages path="/pages/*" />
-    <Contents path="/contents/*" />
-    <Users path="/users/*" />
-    <Error404 default />
-  </Router>
+  <Suspense fallback={<FullPageSpinner />}>
+    <Router>
+      <Dashboard path="/" />
+      <DefinePages path="/define/pages/*" />
+      <DefineTypes path="/define/types/*" />
+      <Pages path="/pages/*" />
+      <Contents path="/contents/*" />
+      <Users path="/users/*" />
+      <Error404 default />
+    </Router>
+  </Suspense>
 );
 
 export default Public;
