903a710e977e7ec1aceffc03fd6e6672834f4a4e
Ian Dela Cruz <iandc76@gmail.com>
2019-10-23 06:47:22 +0800

    fix(admin): Remove lazy loading of private routes
    kinda annoying

--- packages/kdc-cms-admin/src/routes/private/index.js
+++ packages/kdc-cms-admin/src/routes/private/index.js
@@ -1,27 +1,24 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import { Router } from '@reach/router';
-import FullPageSpinner from '../../components/fullPageSpinner';
 
-const Dashboard = React.lazy(() => import('./dashboard'));
-const DefinePages = React.lazy(() => import('./define/pages'));
-const DefineTypes = React.lazy(() => import('./define/types'));
-const Pages = React.lazy(() => import('./pages'));
-const Contents = React.lazy(() => import('./contents'));
-const Users = React.lazy(() => import('./users'));
-const Error404 = React.lazy(() => import('./404'));
+import Dashboard from './dashboard';
+import DefinePages from './define/pages';
+import DefineTypes from './define/types';
+import Pages from './pages';
+import Contents from './contents';
+import Users from './users';
+import Error404 from './404';
 
 const Public = () => (
-  <Suspense fallback={<FullPageSpinner />}>
-    <Router>
-      <Dashboard path="/" />
-      <DefinePages path="/define/pages/*" />
-      <DefineTypes path="/define/types/*" />
-      <Pages path="/pages/*" />
-      <Contents path="/contents/*" />
-      <Users path="/users/*" />
-      <Error404 default />
-    </Router>
-  </Suspense>
+  <Router>
+    <Dashboard path="/" />
+    <DefinePages path="/define/pages/*" />
+    <DefineTypes path="/define/types/*" />
+    <Pages path="/pages/*" />
+    <Contents path="/contents/*" />
+    <Users path="/users/*" />
+    <Error404 default />
+  </Router>
 );
 
 export default Public;
