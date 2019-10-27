93fad7303729e49b3c4571275c1c28b47fc4543a
Ian Dela Cruz <iandc76@gmail.com>
2019-10-28 05:57:25 +0800

    fix(setup): Fix local setup
    issue #16

--- packages/kdc-cms-setup/lib/create-file.js
+++ packages/kdc-cms-setup/lib/create-file.js
@@ -6,21 +6,32 @@ const rootDir = path.resolve(__dirname, "../../../");
 
 const createFile = async ctx => {
   const { stage, aws, jwt_secret, user } = ctx;
+  const configFile = path.resolve(rootDir, `config.${stage}.yml`);
 
   if (aws) {
     const { region, profile, domain, cmsBucket, uploadBucket } = aws;
-    const filename = path.resolve(rootDir, `config.${stage}.yml`);
 
     try {
-      fs.truncateSync(filename);
+      fs.truncateSync(configFile);
     } catch (error) {}
 
     try {
-      fs.appendFileSync(filename, `REGION: ${region}\n`, "utf8");
-      fs.appendFileSync(filename, `PROFILE: ${profile}\n`, "utf8");
-      fs.appendFileSync(filename, `ROOT_DOMAIN: ${domain}\n`, "utf8");
-      fs.appendFileSync(filename, `ADMIN_BUCKET: ${cmsBucket}\n`, "utf8");
-      fs.appendFileSync(filename, `UPLOAD_BUCKET: ${uploadBucket}\n`, "utf8");
+      fs.appendFileSync(configFile, `REGION: ${region}\n`, "utf8");
+      fs.appendFileSync(configFile, `PROFILE: ${profile}\n`, "utf8");
+      fs.appendFileSync(configFile, `ROOT_DOMAIN: ${domain}\n`, "utf8");
+      fs.appendFileSync(configFile, `ADMIN_BUCKET: ${cmsBucket}\n`, "utf8");
+      fs.appendFileSync(configFile, `UPLOAD_BUCKET: ${uploadBucket}\n`, "utf8");
+    } catch (err) {
+      /* Handle the error */
+      return Promise.reject(err);
+    }
+  } else {
+    try {
+      fs.appendFileSync(configFile, `REGION: localhost\n`, "utf8");
+      fs.appendFileSync(configFile, `PROFILE: default\n`, "utf8");
+      fs.appendFileSync(configFile, `ROOT_DOMAIN: localhost\n`, "utf8");
+      fs.appendFileSync(configFile, `ADMIN_BUCKET: admin-local\n`, "utf8");
+      fs.appendFileSync(configFile, `UPLOAD_BUCKET: upload-local\n`, "utf8");
     } catch (err) {
       /* Handle the error */
       return Promise.reject(err);
@@ -29,14 +40,7 @@ const createFile = async ctx => {
 
   if (jwt_secret) {
     try {
-      let filename = "";
-      if (!aws) {
-        filename = path.resolve(rootDir, `config.local.yml`);
-      } else {
-        filename = path.resolve(rootDir, `config.${stage}.yml`);
-      }
-
-      fs.appendFileSync(filename, `JWT_SECRET: ${jwt_secret}\n`, "utf8");
+      fs.appendFileSync(configFile, `JWT_SECRET: ${jwt_secret}\n`, "utf8");
 
       // Set this for local
       process.env.JWT_SECRET = jwt_secret;
