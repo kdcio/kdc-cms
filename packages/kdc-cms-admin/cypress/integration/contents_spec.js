68d8d044a4c0c9e2661ffecb11163fcad00c906b
Ian Dela Cruz <iandc76@gmail.com>
2019-10-31 17:32:44 +0800

    test(admin): Readded previous test
    issue #21

--- packages/kdc-cms-admin/cypress/integration/contents_spec.js
+++ packages/kdc-cms-admin/cypress/integration/contents_spec.js
@@ -29,7 +29,7 @@ describe('Contents', () => {
     cy.get('div.col-sm-2 > .form-control').select('text');
     cy.get('.d-flex > .mr-2').click();
     cy.get(':nth-child(5) > .col-sm-6 > .form-control').type('Slug');
-    cy.get(':nth-child(5) > div.col-sm-2 > .form-control').select('text');
+    cy.get(':nth-child(5) > div.col-sm-2 > .form-control').select('slug');
     cy.get('.d-flex > .mr-2').click();
     cy.get(':nth-child(6) > .col-sm-6 > .form-control').type('Publish Date');
     cy.get(':nth-child(6) > div.col-sm-2 > .form-control').select('date');
@@ -50,7 +50,7 @@ describe('Contents', () => {
     cy.get(':nth-child(4) > .col-sm-6 > .form-control').should('have.value', 'Title');
     cy.get(':nth-child(4) > div.col-sm-2 > .form-control').should('have.value', 'text');
     cy.get(':nth-child(5) > .col-sm-6 > .form-control').should('have.value', 'Slug');
-    cy.get(':nth-child(5) > div.col-sm-2 > .form-control').should('have.value', 'text');
+    cy.get(':nth-child(5) > div.col-sm-2 > .form-control').should('have.value', 'slug');
     cy.get(':nth-child(6) > .col-sm-6 > .form-control').should('have.value', 'Publish Date');
     cy.get(':nth-child(6) > div.col-sm-2 > .form-control').should('have.value', 'date');
     cy.get(':nth-child(7) > .col-sm-6 > .form-control').should('have.value', 'Body');
@@ -76,7 +76,7 @@ describe('Contents', () => {
     cy.get(':nth-child(4) > .col-sm-6 > .form-control').should('have.value', 'Title');
     cy.get(':nth-child(4) > div.col-sm-2 > .form-control').should('have.value', 'text');
     cy.get(':nth-child(5) > .col-sm-6 > .form-control').should('have.value', 'Slug');
-    cy.get(':nth-child(5) > div.col-sm-2 > .form-control').should('have.value', 'text');
+    cy.get(':nth-child(5) > div.col-sm-2 > .form-control').should('have.value', 'slug');
     cy.get(':nth-child(6) > .col-sm-6 > .form-control').should('have.value', 'Publish Date');
     cy.get(':nth-child(6) > div.col-sm-2 > .form-control').should('have.value', 'date');
     cy.get(':nth-child(7) > .col-sm-6 > .form-control').should('have.value', 'Body edited');
@@ -106,32 +106,32 @@ describe('Contents', () => {
     cy.location('pathname').should('eq', '/contents/blogs/add');
     cy.get('.m-0').should('contain', 'Add Blogs');
     cy.get(':nth-child(1) > .col-sm-10 > .form-control').type('My First Blog');
-    cy.get(':nth-child(2) > .col-sm-10 > .form-control').type('my-first-blog');
-    // cy.get(':nth-child(2) > .col-sm-10 > .form-control').should('have.value', 'my-first-blog');
+    cy.get(':nth-child(2) > .col-sm-10 > .form-control').type('my{shift}First{shift}Blog');
+    cy.get(':nth-child(2) > .col-sm-10 > .form-control').should('have.value', 'my-first-blog');
     cy.get(':nth-child(3) > .col-sm-10 > .form-control').type('2019-10-06');
     cy.type_ckeditor('Enim cillum aliqua velit officia nulla dolore duis.');
     cy.get(':nth-child(5) > .col-sm-10 > .form-control').type('Juan Dela Cruz');
     cy.get('form > .btn').click();
 
     cy.location('pathname').should('eq', '/contents/blogs');
-    // cy.get('tbody > tr > :nth-child(1)').should('have.text', 'My First Blog');
+    cy.get('tbody > tr > :nth-child(1)').should('have.text', 'My First Blog');
     cy.get('tbody > tr > :nth-child(2)').should('have.text', '2019-10-06');
     cy.get('.card-header > .btn').click();
 
     cy.location('pathname').should('eq', '/contents/blogs/add');
     cy.get('.m-0').should('contain', 'Add Blogs');
     cy.get(':nth-child(1) > .col-sm-10 > .form-control').type('My Second Blog');
-    cy.get(':nth-child(2) > .col-sm-10 > .form-control').type('my-second-blog');
-    // cy.get(':nth-child(2) > .col-sm-10 > .form-control').should('have.value', 'my-second-blog');
+    cy.get(':nth-child(2) > .col-sm-10 > .form-control').type('my{shift}Second{shift}Blog');
+    cy.get(':nth-child(2) > .col-sm-10 > .form-control').should('have.value', 'my-second-blog');
     cy.get(':nth-child(3) > .col-sm-10 > .form-control').type('2019-10-08');
     cy.type_ckeditor('Nulla ut qui cillum ex minim ullamco sint id id.');
     cy.get(':nth-child(5) > .col-sm-10 > .form-control').type('Juan Dela Cruz');
     cy.get('form > .btn').click();
 
     cy.location('pathname').should('eq', '/contents/blogs');
-    // cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', 'My Second Blog');
+    cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', 'My Second Blog');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').should('have.text', '2019-10-08');
-    // cy.get('tbody > :nth-child(2) > :nth-child(1)').should('have.text', 'My First Blog');
+    cy.get('tbody > :nth-child(2) > :nth-child(1)').should('have.text', 'My First Blog');
     cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', '2019-10-06');
     cy.get(':nth-child(2) > .text-center > .btn-secondary').click();
 
@@ -146,11 +146,11 @@ describe('Contents', () => {
     cy.get(':nth-child(5) > .col-sm-10 > .form-control').should('have.value', 'Juan Dela Cruz');
 
     cy.get(':nth-child(1) > .col-sm-10 > .form-control').type(' edited');
-    cy.get(':nth-child(2) > .col-sm-10 > .form-control').type('-edited');
-    // cy.get(':nth-child(2) > .col-sm-10 > .form-control').should(
-    //   'have.value',
-    //   'my-first-blog-edited'
-    // );
+    cy.get(':nth-child(2) > .col-sm-10 > .form-control').type('{shift}Edited');
+    cy.get(':nth-child(2) > .col-sm-10 > .form-control').should(
+      'have.value',
+      'my-first-blog-edited'
+    );
     cy.get(':nth-child(3) > .col-sm-10 > .form-control')
       .clear()
       .type('2019-10-01');
@@ -161,13 +161,13 @@ describe('Contents', () => {
     cy.get('form > .btn').click();
 
     cy.location('pathname').should('eq', '/contents/blogs');
-    // cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', 'My Second Blog');
+    cy.get('tbody > :nth-child(1) > :nth-child(1)').should('have.text', 'My Second Blog');
     cy.get('tbody > :nth-child(1) > :nth-child(2)').should('have.text', '2019-10-08');
-    // cy.get('tbody > :nth-child(2) > :nth-child(1)').should('have.text', 'My First Blog edited');
+    cy.get('tbody > :nth-child(2) > :nth-child(1)').should('have.text', 'My First Blog edited');
     cy.get('tbody > :nth-child(2) > :nth-child(2)').should('have.text', '2019-10-01');
 
     cy.get(':nth-child(1) > .text-center > .btn-danger').click();
-    // cy.get('tbody > tr > :nth-child(1)').should('have.text', 'My First Blog edited');
+    cy.get('tbody > tr > :nth-child(1)').should('have.text', 'My First Blog edited');
     cy.get('tbody > tr > :nth-child(2)').should('have.text', '2019-10-01');
     cy.get('.btn-secondary').click();
 
